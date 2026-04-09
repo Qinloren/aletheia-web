377d203efa07feae747535e3494b20c5416fd408
qinloren <2336249419@qq.com>
2026-04-09 13:29:18 +0800

    导航条实现

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,47 @@
-import React, {type ReactNode} from "react";
+import React from "react";
 
 import styles from "./index.module.scss";
+import Search from "supercons/Search";
+import {Link } from "react-router-dom";
 
 interface VHeaderProps {
-    children?: ReactNode;
+    title: string;
 }
 
-const VHeader: React.FC<VHeaderProps> = ({ children }) => {
-    return <div className={styles.header}>{children}</div>
+const VHeader: React.FC<VHeaderProps> = (props) => {
+    return <div className={styles.headerContainer}>
+        <div className={styles.headerWarp}>
+            <div className={styles.logoBox}>
+                <div className={styles.logoIcon}></div>
+                <h2 className={styles.logoText}>{props.title}</h2>
+            </div>
+            <div className={styles.menuBox}>
+                <ul className={styles.menu}>
+                    <Link to={"/"}>
+                        <li className={[styles.menuItem, styles.active].join(' ')}>首页</li>
+                    </Link>
+                    <Link to={"/articles"}>
+                        <li className={styles.menuItem}>文章</li>
+                    </Link>
+                    <Link to={"/resources"}>
+                        <li className={styles.menuItem}>资源</li>
+                    </Link>
+                    <Link to={"/qas"}>
+                        <li className={styles.menuItem}>问答</li>
+                    </Link>
+                    <Link to={"/ai"}>
+                        <li className={styles.menuItem}>AI助手</li>
+                    </Link>
+                </ul>
+            </div>
+            <div className={styles.filterBox}>
+                <div className={styles.searchBox}>
+                    <Search className={styles.searchIcon}/>
+                    <input type="text" className={styles.searchInput} placeholder="搜索用户、帖子、内容....."/>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default VHeader;
